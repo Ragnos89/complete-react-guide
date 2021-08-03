24ecb76ea11c788773fef892df5ecc6c5739918e
Ragnos89 <87934506+Ragnos89@users.noreply.github.com>
2021-08-03 21:26:10 +0200

    destructuring ExpenseItem function and changing it to arrow function

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,9 @@
-import './ExpenseItem.css'
+import "./ExpenseItem.css";
 
-function ExpenseItem(props) {
-  const month = props.date.toLocaleString("en-EN",{month: 'long'});
-  const year = props.date.getFullYear();
-  const day = props.date.toLocaleString("en-US",{day: '2-digit'});
+const ExpenseItem = ({ date, title, amount }) => {
+  const month = date.toLocaleString("en-EN", { month: "long" });
+  const year = date.getFullYear();
+  const day = date.toLocaleString("en-US", { day: "2-digit" });
   return (
     <div className="expense-item">
       <div>
@@ -12,11 +12,11 @@ function ExpenseItem(props) {
         <div>{day}</div>
       </div>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className= "expense-item__price">${props.amount}</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
       </div>
     </div>
   );
-}
+};
 
 export default ExpenseItem;
